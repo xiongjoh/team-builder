983c2edaf9cd2b75d888be7be521a76c93bd1dbd
unknown <xiongjoh@gmail.com>
2020-10-13 14:41:38 -0500

    return div from TeamMember.js

--- team-builder/src/TeamMember.js
+++ team-builder/src/TeamMember.js
@@ -1,5 +1,17 @@
 import React from 'react'
 
-export default function TeamMember() {
-    return null
+export default function TeamMember(props) {
+    const {details} = props
+
+    if (!details) {
+        return <h3>Fetching Details...</h3>
+    }
+
+    return (
+        <div className='container'>
+            <h2>{details.username}</h2>
+            <p>Email: {details.email}</p>
+            <p>Role: {details.role}</p>
+        </div>
+    )
 }
